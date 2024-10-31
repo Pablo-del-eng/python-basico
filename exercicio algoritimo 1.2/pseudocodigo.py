f08e7daf522a6bffb76095553ef3318adab48069
Pablo-del-eng <pablopaiva3017@gmail.com>
2024-10-31 10:38:12 -0300

    Add files via upload

---
+++ exercicio algoritimo 1.2/pseudocodigo.py
@@ -0,0 +1,10 @@
+n = input ("Qual o numero a ser multiplicado por 9?" )
+
+n1 = float(n) - 1
+n2 = 10 - float(n)
+
+n3 = float(n1) * 10 + float(n2)
+
+
+
+print (f"O resultado de 9 multiplicado por {n} é igual a {n3}") 
