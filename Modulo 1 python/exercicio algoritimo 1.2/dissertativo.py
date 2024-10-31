ce6f7ac9d0a58d3d5c9c0d7cbe3f653988a0adac
Pablo-del-eng <pablopaiva3017@gmail.com>
2024-10-31 10:37:08 -0300

    Add files via upload

---
+++ Modulo 1 python/exercicio algoritimo 1.2/dissertativo.py
@@ -0,0 +1,6 @@
+Escreva o numero que irá ser multiplicado por 9 e o armazene na variável n. #entrada
+Apos isso subtraia o n por 1, assim descobrindo a variável n1 e a armazene. #processamento
+Apos isso subtraia 10 por n, assim descobrindo a variável n2 e a armazene. #processamento
+Apos isso multiplique n1 por 10 e o some a n2 e descubra a variável n3 e a armazene.#processamento
+Apresente a variável n3, sendo o resultado da linha numero 4. #saída
+
